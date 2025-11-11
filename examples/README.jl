83964d821f09547258f17a325be6dc99267dcd4e
mtfishman <7855256+mtfishman@users.noreply.github.com>
2025-11-11 01:16:41 +0000

    Format .jl files (Runic)

--- examples/README.jl
+++ examples/README.jl
@@ -1,5 +1,5 @@
 # # VisualizationBase.jl
-# 
+#
 # [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://itensor.github.io/VisualizationBase.jl/stable/)
 # [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://itensor.github.io/VisualizationBase.jl/dev/)
 # [![Build Status](https://github.com/ITensor/VisualizationBase.jl/actions/workflows/Tests.yml/badge.svg?branch=main)](https://github.com/ITensor/VisualizationBase.jl/actions/workflows/Tests.yml?query=branch%3Amain)
--- test/test_basics.jl
+++ test/test_basics.jl
@@ -2,14 +2,14 @@ using VisualizationBase: VisualizationBase, @visualize, visualize
 using Test: @test, @testset
 
 @testset "VisualizationBase" begin
-  x = [2, 3]
-  @test sprint(visualize, x) ==
-    "2-element Vector{Int64}:\n 2\n 3\n" ==
-    sprint((io, x) -> display(TextDisplay(io), x), x)
-  @test sprint((io, x) -> visualize(io, x; name="y"), x) ==
-    "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
-  @test sprint((io, x) -> @visualize(x, io=io), x) ==
-    "x = \n 2-element Vector{Int64}:\n  2\n  3\n"
-  @test sprint((io, x) -> @visualize(x, io=io, name="y"), x) ==
-    "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    x = [2, 3]
+    @test sprint(visualize, x) ==
+        "2-element Vector{Int64}:\n 2\n 3\n" ==
+        sprint((io, x) -> display(TextDisplay(io), x), x)
+    @test sprint((io, x) -> visualize(io, x; name = "y"), x) ==
+        "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    @test sprint((io, x) -> @visualize(x, io = io), x) ==
+        "x = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    @test sprint((io, x) -> @visualize(x, io = io, name = "y"), x) ==
+        "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
 end
