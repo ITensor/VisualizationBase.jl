297d15977be5eed503ec3b6a530166707f77f337
mtfishman <7855256+mtfishman@users.noreply.github.com>
2026-02-14 01:46:58 +0000

    Format .jl files (ITensorFormatter)

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -1,15 +1,15 @@
-using VisualizationBase: VisualizationBase, @visualize, visualize
 using Test: @test, @testset
+using VisualizationBase: VisualizationBase, @visualize, visualize
 
 @testset "VisualizationBase" begin
-  x = [2, 3]
-  @test sprint(visualize, x) ==
-    "2-element Vector{Int64}:\n 2\n 3\n" ==
-    sprint((io, x) -> display(TextDisplay(io), x), x)
-  @test sprint((io, x) -> visualize(io, x; name="y"), x) ==
-    "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
-  @test sprint((io, x) -> @visualize(x, io=io), x) ==
-    "x = \n 2-element Vector{Int64}:\n  2\n  3\n"
-  @test sprint((io, x) -> @visualize(x, io=io, name="y"), x) ==
-    "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    x = [2, 3]
+    @test sprint(visualize, x) ==
+        "2-element Vector{Int64}:\n 2\n 3\n" ==
+        sprint((io, x) -> display(TextDisplay(io), x), x)
+    @test sprint((io, x) -> visualize(io, x; name = "y"), x) ==
+        "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    @test sprint((io, x) -> @visualize(x, io = io), x) ==
+        "x = \n 2-element Vector{Int64}:\n  2\n  3\n"
+    @test sprint((io, x) -> @visualize(x, io = io, name = "y"), x) ==
+        "y = \n 2-element Vector{Int64}:\n  2\n  3\n"
 end
